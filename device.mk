278f20148b903d9847e331af0e4882497b75357c
Rajkale99 <kaleraj.rk@gmail.com>
2019-01-08 06:43:34 +0100

    ril: build librmnetctl

--- device.mk
+++ device.mk
@@ -300,6 +300,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# Ril
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
