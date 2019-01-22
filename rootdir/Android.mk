f8050ef34ef4461bdfa9fd7074c90edf0682082c
ShreehariVaasishta <shreehari9481@gmail.com>
2019-01-22 08:32:23 -0500

    i dont remember

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,7 +38,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.rimo02a.usb.rc
+LOCAL_SRC_FILES		:= etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
