52441a84232bc8164014b1226895e18641cc57f1
ShreehariVaasishta <shreehari9481@gmail.com>
2019-01-26 04:12:31 -0500

    remove loggy.sh

--- rootdir/etc/loggy.sh
+++ rootdir/etc/loggy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # loggy.sh.
 
-date=`date +%F_%H-%M-%S`
-logcat -v time -f  /cache/Android_PIE_logcat_${date}.txt
+#date=`date +%F_%H-%M-%S`
+#logcat -v time -f  /cache/Android_PIE_logcat_${date}.txt
 
