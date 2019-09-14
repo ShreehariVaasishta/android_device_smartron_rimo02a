3ae66a7a232a9fc75fda5488c903c0f390b98bc1
Jyotiraditya <dreadnaught02@outlook.com>
2019-09-14 12:11:50 +0530

    remove some

--- device.mk
+++ device.mk
@@ -139,8 +139,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # LeEco Parts
-PRODUCT_PACKAGES += \
-    LeEcoParts
+#PRODUCT_PACKAGES += \
+#    LeEcoParts
 
 # Dolby
 PRODUCT_COPY_FILES +=  \
