b7ea03af2a58810ba396402dd1d5852d8f9c74b7
Akash Mondal <a98mondal@gmail.com>
2019-09-28 15:38:55 +0000

    s2: hidl: build power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -88,7 +88,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.1-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # RenderScript
 PRODUCT_PACKAGES += \
