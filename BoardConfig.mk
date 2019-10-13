44c0286d877380b9b5ba733401e7677c5c36cf6b
ShreehariVaasishta <shreehari9481@gmail.com>
2019-10-13 18:32:03 +0000

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 
 TARGET_KERNEL_SOURCE := kernel/smartron/msm8976
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_CONFIG := lineageos_rimo02a_defconfig
 
 # APEX image
