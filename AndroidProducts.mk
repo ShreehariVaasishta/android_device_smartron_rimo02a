6156764fa1142e167359602f5034a55caf345838
Abhat27 <advaithbhat9@gmail.com>
2019-11-12 07:39:03 +0000

    aex bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,10 +14,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_rimo02a.mk
+    $(LOCAL_DIR)/aosp_rimo02a.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_rimo02a-user \
-    superior_rimo02a-userdebug \
-    superior_rimo02a-eng
+    aosp_rimo02a-user \
+    aosp_rimo02a-userdebug \
+    aosp_rimo02a-eng
 
